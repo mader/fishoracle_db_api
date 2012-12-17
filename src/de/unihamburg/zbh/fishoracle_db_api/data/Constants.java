99407a68a2ad53d59b218c84490652e520787897
Malte Mader <mader@zbh.uni-hamburg.de>
2012-12-17 11:23:22 +0100

    Add some more constants.

--- src/de/unihamburg/zbh/fishoracle_db_api/data/Constants.java
+++ src/de/unihamburg/zbh/fishoracle_db_api/data/Constants.java
@@ -7,16 +7,20 @@ public class Constants {
 	public static final String EXPERIMENT_IDS = "experimentIds";
 	public static final String CNV_STATI = "cnvStati";
 	
-	public static final String SCORE = "score";
+	public static final String DATA_TYPE = "dataType";
+	
+	public static final String SEGMENT_MEAN = "segmentMean";
+	
+	public static final String QUALTY_SCORE = "qualityScore";
 	public static final String SOMATIC = "somatic";
 	public static final String CONFIDENCE = "confidence";
 	public static final String SNP_TOOL = "snpTool";
 	
-	public static final String DATA_TYPE = "dataType";
-	public static final String QUALTY_SCORE = "qualityScore";
-	
-	public static final String SEGMENT_MEAN = "segmentMean";
-	
+	public static final String IS_GLOBAL_SEGMENT_TH = "isGlobalSegmentTh";
 	public static final String GLOBAL_TH = "globalTh";
 	
+	public static final String ENSEMBL_ID = "ensemblId";
+	public static final String ENSEMBL_BIOTYPES = "ensemblBiotypes";
+	public static final String SORTED_SEGMENTS = "sortedSegments";
+	public static final String SHOW_SEGMENT_CAPTION = "showSegmentCaption";
 }
